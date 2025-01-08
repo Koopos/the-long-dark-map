32eb51c24f381ac64f684f1e00387a786166d8ac
娄松 <>
2025-01-08 11:00:21 +0800

    feat: 地图更新

--- app/views/big-bear/big-bear.tsx
+++ app/views/big-bear/big-bear.tsx
@@ -5,7 +5,7 @@ import { MapName } from "~/components/MapName/MapName";
  * @Author: 娄松 
  * @Date: 2025-01-07 10:33:18
  * @LastEditors: 娄松 
- * @LastEditTime: 2025-01-07 15:12:03
+ * @LastEditTime: 2025-01-08 10:51:50
  * @FilePath: \the-long-dark-map\app\full-view\full-view.tsx
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -15,6 +15,8 @@ export default function BigBear() {
             <MapName bg="orange" name="Mystery Lake" path="/mystery-lake" />
             <ConnectName bg="black" name="deep" path="/deep" />
             <MapName bg="green" name="Coastal Highway" path="/coastal-highway" />
+            <MapName bg="green" name="Pleasant Valley" path="/pleasant-valley" />
+            <MapName bg="green" name="Timber Wolf" path="/timber-wolf" />
         </>
 
     )   
---
+++ app/views/timber-wolf/timber-wolf.tsx
@@ -0,0 +1,19 @@
+/*
+ * @Author: 娄松 
+ * @Date: 2025-01-08 10:52:23
+ * @LastEditors: 娄松 
+ * @LastEditTime: 2025-01-08 10:55:11
+ * @FilePath: \the-long-dark-map\app\views\pleasant-valley\pleasant-valley.tsx
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+import { BigMap } from "~/components/BigMap/BigMap";
+import TimberWolfMapPng from './image/map/timber-wolf-map.png'
+
+
+export default function TimberWolf() {
+    return (
+        <BigMap image={TimberWolfMapPng}>
+            <div />
+        </BigMap>
+    )
+}
